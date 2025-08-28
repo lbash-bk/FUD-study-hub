f59a49bf817c390fb99c9346b10f2910c6ba3d27
lbash-bk <blawanbkd@gmail.com>
2025-08-28 22:01:43 +0100

    add service worker

---
+++ accounts/static/js/service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "fud-study-hub-v1";
+const urlsToCache = [
+  "/",                // Homepage
+  "/static/icons/icon-192x192.png",
+  "/static/icons/icon-512x512.png",
+  "/static/manifest.json"
+];
+
+// Install service worker and cache files
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Serve cached files when offline
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Update service worker
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    })
+  );
+});
